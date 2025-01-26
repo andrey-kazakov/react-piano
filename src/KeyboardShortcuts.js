fdac0792efebd37c711b7d4c8cdd7cd3e00fbae9
Andrei Kazakov <nvgator@gmail.com>
2025-01-26 14:07:11 +0200

    KeyboardShortcuts: invert hashes for better readability

--- src/KeyboardShortcuts.js
+++ src/KeyboardShortcuts.js
@@ -15,14 +15,14 @@ function createKeyboardShortcuts({ firstNote, lastNote, keyboardConfig }) {
     const { isAccidental } = MidiNumbers.getAttributes(currentMidiNumber);
     if (isAccidental) {
       keyboardShortcuts.push({
-        key: key.key.flat,
-        code: key.code.flat,
+        key: key.flat.key,
+        code: key.flat.code,
         midiNumber: currentMidiNumber,
       });
     } else {
       keyboardShortcuts.push({
-        key: key.key.natural,
-        code: key.code.natural,
+        key: key.natural.key,
+        code: key.natural.code,
         midiNumber: currentMidiNumber,
       });
       naturalKeyIndex += 1;
@@ -36,41 +36,329 @@ export default {
   create: createKeyboardShortcuts,
   // Preset configurations
   BOTTOM_ROW: [
-    { key: { natural: 'z', flat: 'a' }, code: { natural: 'KeyZ', flat: 'KeyA' } },
-    { key: { natural: 'x', flat: 's' }, code: { natural: 'KeyX', flat: 'KeyS' } },
-    { key: { natural: 'c', flat: 'd' }, code: { natural: 'KeyC', flat: 'KeyD' } },
-    { key: { natural: 'v', flat: 'f' }, code: { natural: 'KeyV', flat: 'KeyF' } },
-    { key: { natural: 'b', flat: 'g' }, code: { natural: 'KeyB', flat: 'KeyG' } },
-    { key: { natural: 'n', flat: 'h' }, code: { natural: 'KeyN', flat: 'KeyH' } },
-    { key: { natural: 'm', flat: 'j' }, code: { natural: 'KeyM', flat: 'KeyJ' } },
-    { key: { natural: ',', flat: 'k' }, code: { natural: 'Comma', flat: 'KeyK' } },
-    { key: { natural: '.', flat: 'l' }, code: { natural: 'Period', flat: 'KeyL' } },
-    { key: { natural: '/', flat: ';' }, code: { natural: 'Slash', flat: 'Semicolon' } },
+    {
+      "natural": {
+        "key": "z",
+        "code": "KeyZ"
+      },
+      "flat": {
+        "key": "a",
+        "code": "KeyA"
+      }
+    },
+    {
+      "natural": {
+        "key": "x",
+        "code": "KeyX"
+      },
+      "flat": {
+        "key": "s",
+        "code": "KeyS"
+      }
+    },
+    {
+      "natural": {
+        "key": "c",
+        "code": "KeyC"
+      },
+      "flat": {
+        "key": "d",
+        "code": "KeyD"
+      }
+    },
+    {
+      "natural": {
+        "key": "v",
+        "code": "KeyV"
+      },
+      "flat": {
+        "key": "f",
+        "code": "KeyF"
+      }
+    },
+    {
+      "natural": {
+        "key": "b",
+        "code": "KeyB"
+      },
+      "flat": {
+        "key": "g",
+        "code": "KeyG"
+      }
+    },
+    {
+      "natural": {
+        "key": "n",
+        "code": "KeyN"
+      },
+      "flat": {
+        "key": "h",
+        "code": "KeyH"
+      }
+    },
+    {
+      "natural": {
+        "key": "m",
+        "code": "KeyM"
+      },
+      "flat": {
+        "key": "j",
+        "code": "KeyJ"
+      }
+    },
+    {
+      "natural": {
+        "key": ",",
+        "code": "Comma"
+      },
+      "flat": {
+        "key": "k",
+        "code": "KeyK"
+      }
+    },
+    {
+      "natural": {
+        "key": ".",
+        "code": "Period"
+      },
+      "flat": {
+        "key": "l",
+        "code": "KeyL"
+      }
+    },
+    {
+      "natural": {
+        "key": "/",
+        "code": "Slash"
+      },
+      "flat": {
+        "key": ";",
+        "code": "Semicolon"
+      }
+    }
   ],
   HOME_ROW: [
-    { key: { natural: 'a', flat: 'q' }, code: { natural: 'KeyA', flat: 'KeyQ' } },
-    { key: { natural: 's', flat: 'w' }, code: { natural: 'KeyS', flat: 'KeyW' } },
-    { key: { natural: 'd', flat: 'e' }, code: { natural: 'KeyD', flat: 'KeyE' } },
-    { key: { natural: 'f', flat: 'r' }, code: { natural: 'KeyF', flat: 'KeyR' } },
-    { key: { natural: 'g', flat: 't' }, code: { natural: 'KeyG', flat: 'KeyT' } },
-    { key: { natural: 'h', flat: 'y' }, code: { natural: 'KeyH', flat: 'KeyY' } },
-    { key: { natural: 'j', flat: 'u' }, code: { natural: 'KeyJ', flat: 'KeyU' } },
-    { key: { natural: 'k', flat: 'i' }, code: { natural: 'KeyK', flat: 'KeyI' } },
-    { key: { natural: 'l', flat: 'o' }, code: { natural: 'KeyL', flat: 'KeyO' } },
-    { key: { natural: ';', flat: 'p' }, code: { natural: 'Semicolon', flat: 'KeyP' } },
-    { key: { natural: "'", flat: '[' }, code: { natural: 'Quote', flat: 'BracketLeft' } },
+    {
+      "natural": {
+        "key": "a",
+        "code": "KeyA"
+      },
+      "flat": {
+        "key": "q",
+        "code": "KeyQ"
+      }
+    },
+    {
+      "natural": {
+        "key": "s",
+        "code": "KeyS"
+      },
+      "flat": {
+        "key": "w",
+        "code": "KeyW"
+      }
+    },
+    {
+      "natural": {
+        "key": "d",
+        "code": "KeyD"
+      },
+      "flat": {
+        "key": "e",
+        "code": "KeyE"
+      }
+    },
+    {
+      "natural": {
+        "key": "f",
+        "code": "KeyF"
+      },
+      "flat": {
+        "key": "r",
+        "code": "KeyR"
+      }
+    },
+    {
+      "natural": {
+        "key": "g",
+        "code": "KeyG"
+      },
+      "flat": {
+        "key": "t",
+        "code": "KeyT"
+      }
+    },
+    {
+      "natural": {
+        "key": "h",
+        "code": "KeyH"
+      },
+      "flat": {
+        "key": "y",
+        "code": "KeyY"
+      }
+    },
+    {
+      "natural": {
+        "key": "j",
+        "code": "KeyJ"
+      },
+      "flat": {
+        "key": "u",
+        "code": "KeyU"
+      }
+    },
+    {
+      "natural": {
+        "key": "k",
+        "code": "KeyK"
+      },
+      "flat": {
+        "key": "i",
+        "code": "KeyI"
+      }
+    },
+    {
+      "natural": {
+        "key": "l",
+        "code": "KeyL"
+      },
+      "flat": {
+        "key": "o",
+        "code": "KeyO"
+      }
+    },
+    {
+      "natural": {
+        "key": ";",
+        "code": "Semicolon"
+      },
+      "flat": {
+        "key": "p",
+        "code": "KeyP"
+      }
+    },
+    {
+      "natural": {
+        "key": "'",
+        "code": "Quote"
+      },
+      "flat": {
+        "key": "[",
+        "code": "BracketLeft"
+      }
+    }
   ],
   QWERTY_ROW: [
-    { key: { natural: 'q', flat: '1' }, code: { natural: 'KeyQ', flat: 'Digit1' } },
-    { key: { natural: 'w', flat: '2' }, code: { natural: 'KeyW', flat: 'Digit2' } },
-    { key: { natural: 'e', flat: '3' }, code: { natural: 'KeyE', flat: 'Digit3' } },
-    { key: { natural: 'r', flat: '4' }, code: { natural: 'KeyR', flat: 'Digit4' } },
-    { key: { natural: 't', flat: '5' }, code: { natural: 'KeyT', flat: 'Digit5' } },
-    { key: { natural: 'y', flat: '6' }, code: { natural: 'KeyY', flat: 'Digit6' } },
-    { key: { natural: 'u', flat: '7' }, code: { natural: 'KeyU', flat: 'Digit7' } },
-    { key: { natural: 'i', flat: '8' }, code: { natural: 'KeyI', flat: 'Digit8' } },
-    { key: { natural: 'o', flat: '9' }, code: { natural: 'KeyO', flat: 'Digit9' } },
-    { key: { natural: 'p', flat: '0' }, code: { natural: 'KeyP', flat: 'Digit0' } },
-    { key: { natural: '[', flat: '-' }, code: { natural: 'BracketLeft', flat: 'Minus' } },
+    {
+      "natural": {
+        "key": "q",
+        "code": "KeyQ"
+      },
+      "flat": {
+        "key": "1",
+        "code": "Digit1"
+      }
+    },
+    {
+      "natural": {
+        "key": "w",
+        "code": "KeyW"
+      },
+      "flat": {
+        "key": "2",
+        "code": "Digit2"
+      }
+    },
+    {
+      "natural": {
+        "key": "e",
+        "code": "KeyE"
+      },
+      "flat": {
+        "key": "3",
+        "code": "Digit3"
+      }
+    },
+    {
+      "natural": {
+        "key": "r",
+        "code": "KeyR"
+      },
+      "flat": {
+        "key": "4",
+        "code": "Digit4"
+      }
+    },
+    {
+      "natural": {
+        "key": "t",
+        "code": "KeyT"
+      },
+      "flat": {
+        "key": "5",
+        "code": "Digit5"
+      }
+    },
+    {
+      "natural": {
+        "key": "y",
+        "code": "KeyY"
+      },
+      "flat": {
+        "key": "6",
+        "code": "Digit6"
+      }
+    },
+    {
+      "natural": {
+        "key": "u",
+        "code": "KeyU"
+      },
+      "flat": {
+        "key": "7",
+        "code": "Digit7"
+      }
+    },
+    {
+      "natural": {
+        "key": "i",
+        "code": "KeyI"
+      },
+      "flat": {
+        "key": "8",
+        "code": "Digit8"
+      }
+    },
+    {
+      "natural": {
+        "key": "o",
+        "code": "KeyO"
+      },
+      "flat": {
+        "key": "9",
+        "code": "Digit9"
+      }
+    },
+    {
+      "natural": {
+        "key": "p",
+        "code": "KeyP"
+      },
+      "flat": {
+        "key": "0",
+        "code": "Digit0"
+      }
+    },
+    {
+      "natural": {
+        "key": "[",
+        "code": "BracketLeft"
+      },
+      "flat": {
+        "key": "-",
+        "code": "Minus"
+      }
+    }
   ],
 };
